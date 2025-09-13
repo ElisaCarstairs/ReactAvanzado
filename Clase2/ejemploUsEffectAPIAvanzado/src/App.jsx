4f91fb22014aecaed5e42c89f723ffbd420b4db3
ElisaCarstairs <marina.arredondo.h@gmail.com>
2025-09-12 23:01:11 -0600

    Añadir componentes

--- Clase2/ejemploUsEffectAPIAvanzado/src/App.jsx
+++ Clase2/ejemploUsEffectAPIAvanzado/src/App.jsx
@@ -3,8 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import fetchPosts from '../Services/post.js' //Importar la función del otro archivo
+import Container from '../components/Container'
+import PostDetail from '../components/PostDetail'
+
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [selectedPostId, setSelectedPostId] = useState(null)
   const [posts, setPosts] = useState([])
   useEffect(() => {
     
@@ -16,12 +21,11 @@ function App() {
   }, [])
   return (
     
-    <>
-      {posts.map((post) => 
-        <p key ={post.id} >{post.title}</p>
-      )}
-      
-    </>
+    <div>
+      <Container posts={posts} handleClick={handleClick} />
+      {selectedPost && <PostDetail post={selectedPost} />}
+      <PostDetailAlt id={selectedPostId} />
+    </div>
   )
 }
 
