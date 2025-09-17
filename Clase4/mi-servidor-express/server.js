b7b859ba97d10cde825fb6f66fbf9885e54727ed
ElisaCarstairs <marina.arredondo.h@gmail.com>
2025-09-16 23:34:40 -0600

    Añadir servidor express con 'Hola mundo'

---
+++ Clase4/mi-servidor-express/server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Endpoint raíz que responde 'Hola Mundo'
+app.get('/', (req, res) => {
+  res.send('Hola Mundo');
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
